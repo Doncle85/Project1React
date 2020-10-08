1c57692d8b7b20b77998b0c2683c5cffc195ba6b
az02754 <clement.c.malarme@renault.com>
2020-10-08 11:52:16 +0200

    Ajout state Livre

--- src/containers/Livres/Livres.js
+++ src/containers/Livres/Livres.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import Bouton from "../../components/Bouton/Bouton"
 
 class Livres extends Component {
+    state = {
+        livres : [
+            {id:1, titre:"la vie de reve", auteur:"nola malcom", nbPages:500},
+            {id:2, titre:"Je prete pas", auteur:"Lyly malcom", nbPages:500},
+            {id:3, titre:"I have a dream", auteur:"Nola malcom", nbPages:500},
+            {id:4, titre:"Meme pas mal", auteur:"Alyssa malcom", nbPages:500},
+        ]
+    }
     render() {
         return (
             <table className="table text-center">
@@ -14,13 +22,24 @@ class Livres extends Component {
                 </tr>
                 </thead>
                 <tbody>
-                <td>Titres1</td>
-                <td>Auteur1</td>
-                <td>100</td>
-                <td><Bouton typeBtn="btn-warning" clic={() => console.log("modification")}>Modifier</Bouton></td>
-                <td><Bouton typeBtn="btn-danger" clic={() => console.log("suppression")}>Supprimer</Bouton></td>
-                </tbody>
-            </table>
+                {
+                    this.state.livres.map(livre => {
+                    return(
+                        <tr key={livre.id}>
+                        <td>{livre.titre}</td>
+                        <td>{livre.auteur}</td>
+                        <td>{livre.nbPages}</td>
+                        <td><Bouton typeBtn="btn-warning" clic={() => console.log("modification")}>Modifier</Bouton></td>
+                        <td><Bouton typeBtn="btn-danger" clic={() => console.log("suppression")}>Supprimer</Bouton></td>
+                        </tr>
+
+                    );
+
+                })
+                }
+                    </tbody>
+                    </table>
+
         )
     }
 }
