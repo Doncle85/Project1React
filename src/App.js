5271ac5a09888f5d1a631083b3fc860c9ea6c1b7
az02754 <clement.c.malarme@renault.com>
2020-10-08 10:49:42 +0200

    Ajout component bouton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import TitreH1 from "./components/Titres/TitreH1";
-
+import Bouton from "./components/Bouton/Bouton";
 
 function App() {
   return (
    <div className="container">
      <TitreH1>Pages listant les livres</TitreH1>
        <div>Livres</div>
-       <button>Ajouter</button>
+       <Bouton typeBtn="btn-danger" clic={() => console.log("AJOUT")}>Ajouter</Bouton>
+       <Bouton typeBtn="btn-success" clic={() => console.log("Modifier")}>Modifier</Bouton>
+       <Bouton typeBtn="btn-warning" clic={() => console.log("SUPPROIMER")}>Supprimer</Bouton>
    </div>
   );
 }
