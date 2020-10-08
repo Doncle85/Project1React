4b3aa670e621229a079df2730249f4bcc5801252
az02754 <clement.c.malarme@renault.com>
2020-10-08 17:49:12 +0200

    Modification du bouton d'ajout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TitreH1 from "./components/Titres/TitreH1";
 import Bouton from "./components/Bouton/Bouton";
 import Livres from "./containers/Livres/Livres";
 
-function App() {
-  return (
-   <div className="container">
-     <TitreH1>Pages listant les livres</TitreH1>
-       <Livres/>
-       <Bouton typeBtn="btn-success"
-               css="w-100"
-               clic={() => console.log("AJOUT")}>Ajouter
-       </Bouton>
-   </div>
-  );
+class App extends Component {
+    state = {
+        ajoutLivre : false
+    }
+
+    handleClicAjoutLivre = () => {
+        this.setState((oldState, props) => {
+            return{ajoutLivre: !oldState.ajoutLivre}
+        }) ;
+    }
+
+  render() {
+      return (
+          <div className="container">
+              <TitreH1>Pages listant les livres</TitreH1>
+              <Livres/>
+              <Bouton typeBtn="btn-success"
+                      css="w-100"
+                      clic={this.handleClicAjoutLivre}>{ !this.state.ajoutLivre ? "Ajouter" :"Feemer l'ajout"}
+              </Bouton>
+          </div>
+      );
+  }
 }
 
 export default App;
